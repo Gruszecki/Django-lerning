6efde36f797663f5e46d25bc696dcd4e5652b9f3
Gruszecki <wojtek.gruszecki@gmail.com>
2022-05-10 20:48:49 +0200

    Add model Film

--- filmy/models.py
+++ filmy/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=64, blank=False)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    desc = models.TextField(default='Bardzo ciekawy film o miłości.')
+    rating_imdb = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+
+    def __str__(self):
+        return self.title_and_date()
+
+    def title_and_date(self):
+        return f'{self.title} ({self.year})'
