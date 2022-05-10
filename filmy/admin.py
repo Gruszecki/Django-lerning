32baa56a155cd7bccae54797b0085389b55c5495
Gruszecki <wojtek.gruszecki@gmail.com>
2022-05-10 21:28:47 +0200

    Custom admin page

--- filmy/admin.py
+++ filmy/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Film
 
 
-admin.site.register(Film)
+# admin.site.register(Film)
 
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['title', 'year', 'rating_imdb']
+    list_filter = ['year']
+    search_fields = ['title', 'desc']
