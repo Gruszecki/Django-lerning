6fbf1623ba558abf5e8502bca57e3c0df18a60dc
Gruszecki <wojtek.gruszecki@gmail.com>
2022-05-27 19:28:37 +0200

    Hide sensitive data

--- learning/settings.py
+++ learning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jgpsv2co!5i534azz@0l#vk3k9@((-43af5&lli_e-=i+h8sh)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
